b81ad0b5544dfa536bddd51652c34cb3180c7f30
wenpo huang <he89302@gmail.com>
2019-09-03 20:21:49 +0800

    intergration test about move & remove stage on the board.

--- src/test/java/com/practice/cleankanban/usecase/kanbanboard/board/ReorderStageOfBoardTest.java
+++ src/test/java/com/practice/cleankanban/usecase/kanbanboard/board/ReorderStageOfBoardTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import com.practice.cleankanban.adapter.gateway.kanbanboard.InMemoryBoardRepository;
 import com.practice.cleankanban.adapter.gateway.kanbanboard.InMemoryStageRepository;
+import com.practice.cleankanban.adapter.presenter.RemoveStagePresenter;
 import com.practice.cleankanban.adapter.presenter.kanbanboard.MoveStageOfBoardMessagePresenter;
 import com.practice.cleankanban.adapter.presenter.kanbanboard.MultipleStagePresenter;
 import com.practice.cleankanban.domain.model.kanbanboard.stage.AbstractDomainEventTest;
@@ -18,6 +19,10 @@ import com.practice.cleankanban.usecase.kanbanboard.stage.get.GetStagesOutput;
 import com.practice.cleankanban.usecase.kanbanboard.stage.get.GetStagesUseCase;
 import com.practice.cleankanban.usecase.kanbanboard.stage.get.GetStagesUseCaseImpl;
 
+import com.practice.cleankanban.usecase.kanbanboard.stage.remove.RemoveStageInput;
+import com.practice.cleankanban.usecase.kanbanboard.stage.remove.RemoveStageOutput;
+import com.practice.cleankanban.usecase.kanbanboard.stage.remove.RemoveStageUseCase;
+import com.practice.cleankanban.usecase.kanbanboard.stage.remove.RemoveStageUseCaseImpl;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -115,7 +120,7 @@ public class ReorderStageOfBoardTest extends AbstractDomainEventTest {
     }
 
     @Test
-    public void move_test_at_position_4_to_same_position() {
+    public void move_test_at_position_4_to_same_position_then_remove_test_stage_then_move_ready_at_position_1_to_position_5() {
         MoveStageOfBoardUseCase useCase = new MoveStageOfBoardUseCaseImpl(boardRepository, stageRepository);
         MoveStageOfBoardInput input = MoveStageOfBoardUseCaseImpl.createInput();
         MoveStageOfBoardOutput output = new MoveStageOfBoardMessagePresenter();
@@ -141,5 +146,44 @@ public class ReorderStageOfBoardTest extends AbstractDomainEventTest {
         assertEquals(4, getStagesOutput.getStages().get(3).getOrdering());//test
         assertEquals(5, getStagesOutput.getStages().get(4).getOrdering());//fixBug
         assertEquals(6, getStagesOutput.getStages().get(5).getOrdering());//deploy
+
+        RemoveStageUseCase removeStageUseCase = new RemoveStageUseCaseImpl(boardRepository, stageRepository);
+        RemoveStageInput removeStageInput = RemoveStageUseCaseImpl.createInput();
+        RemoveStageOutput removeStageOutput = new RemoveStagePresenter();
+
+        removeStageInput.setBoardId(boardId);
+        removeStageInput.setStageId(stageRepository.findByStageName("test").getId());
+
+        removeStageUseCase.execute(removeStageInput, removeStageOutput);
+
+        getStagesUseCase.execute(getStagesInput, getStagesOutput);
+
+        assertEquals(1, getStagesOutput.getStages().get(0).getOrdering());//ready
+        assertEquals(2, getStagesOutput.getStages().get(1).getOrdering());//analysis
+        assertEquals(3, getStagesOutput.getStages().get(2).getOrdering());//development
+        assertEquals(4, getStagesOutput.getStages().get(3).getOrdering());//fixBug
+        assertEquals(5, getStagesOutput.getStages().get(4).getOrdering());//deploy
+        //assertEquals(6, getStagesOutput.getStages().get(5).getOrdering());//test
+        assertEquals("ready", getStagesOutput.getStages().get(0).getName());//ready
+        assertEquals("analysis", getStagesOutput.getStages().get(1).getName());//analysis
+        assertEquals("development", getStagesOutput.getStages().get(2).getName());//development
+        assertEquals("fixBug", getStagesOutput.getStages().get(3).getName());//fixBug
+        assertEquals("deploy", getStagesOutput.getStages().get(4).getName());//deploy
+        //assertEquals("test", getStagesOutput.getStages().get(5).getName());//test
+
+        input.setBoardId(boardId);
+        input.setStageId(stageRepository.findByStageName("ready").getId());
+        input.setOldPosition(1);
+        input.setNewPosition(5);
+
+        useCase.execute(input, output);
+
+        getStagesUseCase.execute(getStagesInput, getStagesOutput);
+
+        assertEquals(1, getStagesOutput.getStages().get(1).getOrdering());//analysis
+        assertEquals(2, getStagesOutput.getStages().get(2).getOrdering());//development
+        assertEquals(3, getStagesOutput.getStages().get(3).getOrdering());//fixBug
+        assertEquals(4, getStagesOutput.getStages().get(4).getOrdering());//deploy
+        assertEquals(5, getStagesOutput.getStages().get(0).getOrdering());//ready
     }
 }
